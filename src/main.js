bc5cd7abd03582df49fc18373225518cdc5eb74f
Маша <masha.kustova-guselnikova@yandex.ru>
2020-11-22 04:39:21 +0500

    Рендерит компоненты

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import {createTripInfoTemplate} from './view/trip-info';
 import {createMenuTemplate} from './view/menu';
+import {createFiltersTemplate} from './view/filters';
+import {createSortingTemplate} from './view/sorting';
+import {createListTemplate} from './view/list';
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -10,6 +13,14 @@ const tripMain = siteBodyElement.querySelector(`.trip-main`);
 
 render(tripMain, createTripInfoTemplate(), `afterbegin`);
 
-const menu = tripMain.querySelector(`.trip-controls`);
+const controls = tripMain.querySelector(`.trip-controls`);
 
-render(menu, createMenuTemplate(), `afterbegin`);
+render(controls.children[0], createMenuTemplate(), `afterend`);
+
+render(controls, createFiltersTemplate(), `beforeend`);
+
+const tripEvents = siteBodyElement.querySelector(`.trip-events`);
+
+render(tripEvents.children[0], createSortingTemplate(), `afterend`);
+
+render(tripEvents, createListTemplate(), `beforeend`);
