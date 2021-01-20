e5f32372058e828fdfae74009fc7c6b7a38509f5
Маша <masha.kustova-guselnikova@yandex.ru>
2021-01-20 08:35:12 +0500

    Замыкает датабиндинг

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -4,7 +4,7 @@ import Sort from '../view/sorting';
 import ListView from '../view/list';
 import PointPresenter from '../presenter/point';
 import {sortPointsUpDay, sortPointsDownDuration, sortPointsDownPrice} from '../utils/common';
-import {SortType} from '../utils/const';
+import {SortType, UpdateType, UserAction} from '../utils/const';
 
 export default class Trip {
   constructor(tripContainer, pointsModel) {
@@ -52,19 +52,32 @@ export default class Trip {
   }
 
   _handleViewAction(actionType, updateType, update) {
-    console.log(actionType, updateType, update);
-    // Здесь будем вызывать обновление модели.
-    // actionType - действие пользователя, нужно чтобы понять, какой метод модели вызвать
-    // updateType - тип изменений, нужно чтобы понять, что после нужно обновить
-    // update - обновленные данные
+    switch (actionType) {
+      case UserAction.UPDATE_POINT:
+        this._pointsModel.updatePoint(updateType, update);
+        break;
+      case UserAction.ADD_TASK:
+        this._pointsModel.addPoint(updateType, update);
+        break;
+      case UserAction.DELETE_TASK:
+        this._pointsModel.deletePoint(updateType, update);
+        break;
+    }
   }
 
   _handleModelEvent(updateType, data) {
-    console.log(updateType, data);
-    // В зависимости от типа изменений решаем, что делать:
-    // - обновить часть списка (например, когда поменялось описание)
-    // - обновить список (например, когда задача ушла в архив)
-    // - обновить всю доску (например, при переключении фильтра)
+    switch (updateType) {
+      case UpdateType.PATCH:
+        // - обновить часть списка (например, когда поменялось описание)
+        this._pointPresenter[data.id].init(data);
+        break;
+      case UpdateType.MINOR:
+        // - обновить список (например, когда задача ушла в архив)
+        break;
+      case UpdateType.MAJOR:
+        // - обновить всю доску (например, при переключении фильтра)
+        break;
+    }
   }
 
   _handleSortTypeChange(sortType) {
